3e905d90b2722888ad1668e2df61324ffb37e19b
Evan Quan <evanlquan@gmail.com>
2019-02-03 03:22:19 -0700

    Add initialize methods to UserInterface (incomplete)

--- Headers/UserInterface.h
+++ Headers/UserInterface.h
@@ -1,5 +1,9 @@
 #pragma once
 #include "stdafx.h"
+#include "Shader.h"
+
+#define DISPLAY_COUNT_MIN 0
+#define DISPLAY_COUNT_MAX 4
 /*
 An interface to all user interface features.
 
@@ -9,9 +13,10 @@ class UserInterface
 {
 public:
 	static UserInterface* getInstance(GLFWwindow* window);
+
 	~UserInterface();
 
-	void setPlayerCount(int count);
+	void setDisplayCount(int count);
 	void addScore(int joystickID, int score);
 	void subtractScore(int joystickID, int score);
 	void resetScore(int joystickID);
@@ -21,6 +26,11 @@ private:
 	static UserInterface* m_pInstance;
 
 	void setScore(int joystickID, int score);
+	void renderText(Shader &shader, string text, GLfloat x, GLfloat y, GLfloat scale, vec3 color);
+	void initializeScores();
+	void initializeUserInterface();
+	void initializeDisplayCount();
+	void initializeCooldowns();
 
 	/// Holds all state information relevant to a character as loaded using FreeType
 	struct Character {
@@ -30,7 +40,11 @@ private:
 		GLuint Advance;     // Horizontal offset to advance to next glyph
 	};
 
-	std::map<GLchar, Character> Characters;
+	std::map<GLchar, Character> characters;
+
+	std::map<int, int> scores;
+
+	int displayCount;
 
 	// Window reference
 	GLFWwindow* m_pWindow;
--- Source/UserInterface.cpp
+++ Source/UserInterface.cpp
@@ -7,6 +7,9 @@ UserInterface* UserInterface::m_pInstance = nullptr;
 UserInterface::UserInterface(GLFWwindow* window)
 {
 	m_pWindow = window;
+	displayCount = 0;
+	initializeUserInterface();
+
 }
 
 UserInterface* UserInterface::getInstance(GLFWwindow* window)
@@ -24,3 +27,103 @@ UserInterface::~UserInterface()
 	m_pWindow = nullptr;
 }
 
+/*
+Set the number of UIs to display
+*/
+void UserInterface::setDisplayCount(int count)
+{
+	if (DISPLAY_COUNT_MIN < count)
+	{
+		count = DISPLAY_COUNT_MIN;
+	}
+	else if (DISPLAY_COUNT_MAX > count)
+	{
+		count = DISPLAY_COUNT_MAX;
+	}
+}
+
+/*
+Initialize all aspects of the UI. This should be done at the start of every game.
+*/
+void UserInterface::initializeUserInterface()
+{
+	initializeScores();
+	initializeCooldowns();
+}
+
+/*
+Initialize the scores of all players to 0
+*/
+void UserInterface::initializeScores()
+{
+	for (int i = DISPLAY_COUNT_MIN; i < DISPLAY_COUNT_MAX; i++)
+	{
+		setScore(i, 0);
+	}
+}
+
+/*
+Initialize the cool down counts for all abilities
+*/
+void UserInterface::initializeCooldowns()
+{
+	for (int i = DISPLAY_COUNT_MIN; i < DISPLAY_COUNT_MAX; i++)
+	{
+		// TODO
+	}
+}
+
+void UserInterface::setScore(int joystickID, int score)
+{
+	scores.insert(pair<int, int>(joystickID, score));
+	// TODO
+	// make this render
+}
+
+
+// https://learnopengl.com/code_viewer.php?code=in-practice/text_rendering
+// void UserInterface::renderText(Shader &shader, std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color)
+// {
+	// Activate corresponding render state	
+	// TODO shader stuff  here
+	// shader.Use();
+	// glUniform3f(glGetUniformLocation(shader.Program, "textColor"), color.x, color.y, color.z);
+	// glActiveTexture(GL_TEXTURE0);
+	// glBindVertexArray(VAO);
+	
+	// Iterate through all characters
+//	std::string::const_iterator c;
+//	for (c = text.begin(); c != text.end(); c++) 
+//	{
+//		Character ch = characters[*c];
+//		
+//		GLfloat xpos = x + ch.Bearing.x * scale;
+//		GLfloat ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+//		
+//		GLfloat w = ch.Size.x * scale;
+//		GLfloat h = ch.Size.y * scale;
+//		// Update VBO for each character
+//		GLfloat vertices[6][4] = {
+//			{ xpos,     ypos + h,   0.0, 0.0 },            
+//			{ xpos,     ypos,       0.0, 1.0 },
+//			{ xpos + w, ypos,       1.0, 1.0 },
+//			
+//			{ xpos,     ypos + h,   0.0, 0.0 },
+//			{ xpos + w, ypos,       1.0, 1.0 },
+//			{ xpos + w, ypos + h,   1.0, 0.0 }           
+//		};
+//		// Render glyph texture over quad
+//		glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+//		// Update content of VBO memory
+//		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+//		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // Be sure to use glBufferSubData and not glBufferData
+//		
+//		glBindBuffer(GL_ARRAY_BUFFER, 0);
+//		// Render quad
+//		glDrawArrays(GL_TRIANGLES, 0, 6);
+//		// Now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+//		x += (ch.Advance >> 6) * scale; // Bitshift by 6 to get value in pixels (2^6 = 64 (divide amount of 1/64th pixels by 64 to get amount of pixels))
+//	}
+//	glBindVertexArray(0);
+//	glBindTexture(GL_TEXTURE_2D, 0);
+// }
