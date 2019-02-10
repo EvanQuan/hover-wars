7b6888672699281302a8bc489ce6f0bc4c7e3287
Evan Quan <evanlquan@gmail.com>
2019-02-09 21:32:17 -0700

    Add eAbility

---
+++ Headers/Enums/eAbility.h
@@ -0,0 +1,15 @@
+#pragma once
+
+/*
+Represents an ability. Abilities can be activated and have a cool down. 
+*/
+enum eAbility
+{
+	ABILITY_ROCKET,
+	ABILITY_SPIKES,
+	ABILITY_TRAIL,
+	DASH_BACK,
+	DASH_FORWARD,
+	DASH_LEFT,
+	DASH_RIGHT,
+};
