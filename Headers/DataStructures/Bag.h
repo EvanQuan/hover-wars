218bae9bdf5061f0fce2aa2d79f3efba7b2b3d35
Evan Quan <evanlquan@gmail.com>
2019-02-10 02:13:31 -0700

    Add more Bag documentation

--- Headers/DataStructures/Bag.h
+++ Headers/DataStructures/Bag.h
@@ -7,11 +7,19 @@ using namespace std;
 /*
 Bags contain quantities of objects, which can be added or removed.
 
+Stuff with compilation and linking with template classes:
+https://www.codeproject.com/Articles/48575/How-to-define-a-template-class-in-a-h-file-and-imp
+None of these worked for me, so I decided to go with...
+
 Entirely class is defined in header, instead of .cpp file due to template usage.
 https://stackoverflow.com/questions/1353973/c-template-linking-error 
 
-Use of map instead of unordered map makees it easier for pairs to be key.
+Use of map instead of unordered map makes it easier for pairs to be key.
 https://www.techiedelight.com/use-std-pair-key-std-unordered_map-cpp/
+
+The exercise of reimplementing this from Java to C++ has made me learn just
+how different Java's generics are from C++'s templates. Holy hell this was
+a pain in the ass for something that looks so simple.
 */
 template <class K>
 class Bag
