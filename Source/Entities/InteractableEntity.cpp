2232e26da59d0bd3258e57a7f7faa28731c5eea5
James Cote <james.c.cote@gmail.com>
2019-02-13 12:13:22 -0700

    Added Flame Sprite Sheet, testing with Interactable Entity

--- Source/Entities/InteractableEntity.cpp
+++ Source/Entities/InteractableEntity.cpp
@@ -32,10 +32,14 @@ void InteractableEntity::loadAsBillboard(const vec3* vNormal, int iHeight, int i
 {
     // Load Render Component and get Texture Dimensions
     vec2 vUVStart = vec2(0.0f);
-    vec2 vUVEnd = vec2(1.0f);
+    vec2 vUVEnd = vec2(0.125f);
+
+    Material pBillboardMaterial;
+    pBillboardMaterial.fShininess = 0.0f;
+    pBillboardMaterial.sDiffuseMap = "textures/matty-wyett-simmonds-flamevfxlooping.jpg";
 
     // Generate the Mesh
-    m_pBillboardMesh = MESH_MANAGER->generateBillboardMesh(m_vPosition, *vNormal, vUVStart, vUVEnd, iHeight, iWidth, pMaterial, this);
+    m_pBillboardMesh = MESH_MANAGER->generateBillboardMesh(m_vPosition, *vNormal, vUVStart, vUVEnd, iHeight, iWidth, &pBillboardMaterial, this);
 
     // Generate the Render Component
     m_pRenderComponent = ENTITY_MANAGER->generateRenderComponent(m_iID, m_pBillboardMesh, true, ShaderManager::eShaderType::BILLBOARD_SHDR, GL_POINTS);
--- Source/EntityManager.cpp
+++ Source/EntityManager.cpp
@@ -62,7 +62,7 @@ void EntityManager::initializeEnvironment(string sFileName)
 
     // TESTING: To Be Removed
     vec3 vNormal(0.0f, 1.0f, 0.0f);
-    vec3 vPosition(10.0f, 10.0f, 5.0f);
+    vec3 vPosition(5.0f, 5.0f, 5.0f);
     unique_ptr<InteractableEntity> pTestingEntity = make_unique<InteractableEntity>(getNewEntityID(), &vPosition);
     pTestingEntity->loadAsBillboard(&vNormal, 2, 1, nullptr);
     m_pBillboardTesting = pTestingEntity.get();
