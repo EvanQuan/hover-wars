e8fed2a83b7f9dd59e744bbae942fb7144e16367
Evan Quan <evanlquan@gmail.com>
2019-02-19 00:08:51 -0700

    Add FuncUtils::contains and FuncUtils::getValueIfNotDefault for unordered_map

--- Headers/Utils/FuncUtils.h
+++ Headers/Utils/FuncUtils.h
@@ -27,6 +27,23 @@ namespace FuncUtils
     {
         return m.find(key) != m.end();
     }
+
+    /*
+    Check if an unordered map contains a specified key.
+
+    This is a standard method of map in C++20. I can feel the disapointment as
+    you look over this James, since this is such a trivially unnecessary
+    function.
+
+    @param unordered_map<K, V>  m to check if the key exists
+    @param K                    key to check in map
+    @return true if the specified unordered_map contains the specified key.
+    */
+    template <class K, class V>
+    bool contains(unordered_map<K, V> m, K key)
+    {
+        return m.find(key) != m.end();
+    }
     /*
     Retrieve the value from the specified key in a given map. If the key/value
     pair does not exist, return the default value instead.
@@ -38,6 +55,21 @@ namespace FuncUtils
     */
     template <class K, class V>
     V getValueIfNotDefault(map<K, V> m, K key, V defaultValue)
+    {
+        return contains(m, key) ? m[key] : defaultValue;
+    }
+
+    /*
+    Retrieve the value from the specified key in a given unordered_map. If the
+    key/value pair does not exist, return the default value instead.
+
+    @param unordered_map<K, V>  m to check if key exists
+    @param K                    key to check in map
+    @param V                    defaultValue to return if key not present in m
+    @return value to key if present, or default if not present
+    */
+    template <class K, class V>
+    V getValueIfNotDefault(unordered_map<K, V> m, K key, V defaultValue)
     {
         return contains(m, key) ? m[key] : defaultValue;
     }
