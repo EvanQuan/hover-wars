54cf264a0cfe1b3ad02b8623612b096bb082fe8d
Evan Quan <evanlquan@gmail.com>
2019-03-25 11:09:19 -0600

    Fire recharge is lower, duration is 5 seconds

--- Headers/DataStructures/SpriteSheetDatabase.h
+++ Headers/DataStructures/SpriteSheetDatabase.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "stdafx.h"
 
+#define FLAME_DURATION 5.0f
 // Name: SpriteSheetDatabase
 // Written by: James Coté
 // Description: Contains a hardcoded database of Sprite information that can be used
@@ -19,7 +20,7 @@ namespace SpriteSheetDatabase
     // Database
     const vector<sSpriteSheetInfo> vSpriteInformation{
         { "textures/matty-wyett-simmonds-flamevfxlooping.jpg",  // FIRE_SPRITE
-           vec2(0.125f, 0.125f), vec2(0.005f, 0.0f), 8, 8, 20.0f },
+           vec2(0.125f, 0.125f), vec2(0.005f, 0.0f), 8, 8, FLAME_DURATION },
         { "textures/Smoke15Frames.png",                         // SMOKE_SPRITE
            vec2(0.2f, 0.3333333f), vec2(0.0f), 5, 3, 15.0f }
     };
