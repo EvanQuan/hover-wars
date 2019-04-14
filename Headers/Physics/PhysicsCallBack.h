0bd3c891ab30ca5f9eced73056152c3b6fd0ecbf
Evan Quan <evanlquan@gmail.com>
2019-04-13 20:34:44 -0600

    Remove unused actor name macros

--- Headers/Physics/PhysicsCallBack.h
+++ Headers/Physics/PhysicsCallBack.h
@@ -30,16 +30,4 @@ public:
 private:
     PxRigidDynamic *m_pBody1 = NULL;
     PxRigidDynamic *m_pBody2 = NULL;
-
-    std::unordered_map<char, eHovercraft> ownerToHovercraft =
-    {
-        {C_OWNER_PLAYER_1,  HOVERCRAFT_PLAYER_1},
-        {C_OWNER_PLAYER_2,  HOVERCRAFT_PLAYER_2},
-        {C_OWNER_PLAYER_3,  HOVERCRAFT_PLAYER_3},
-        {C_OWNER_PLAYER_4,  HOVERCRAFT_PLAYER_4},
-        {C_OWNER_BOT_1,     HOVERCRAFT_BOT_1},
-        {C_OWNER_BOT_2,     HOVERCRAFT_BOT_2},
-        {C_OWNER_BOT_3,     HOVERCRAFT_BOT_3},
-        {C_OWNER_BOT_4,     HOVERCRAFT_BOT_4},
-    };
 };
